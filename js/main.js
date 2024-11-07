e1615bbc64a1c49004a1ae3fca39a7a4b321afa5
Tania <nadsanp@gmail.com>
2024-11-07 10:22:53 +0100

    create filtered-series-list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 "use strict";
+
+/* Búsqueda de la serie en el servidor
+Cuando la usuaria haga click en buscar:
+    Se recoge el dato del casillero
+    Se construye la url
+    Se hace la petición al servidor
+    Por cada serie contenida en el resultado
+        Pinto la serie en la página
+
+1- Selecciono los elementos HTML (input de búsqueda,botón de buscar y sección para pintar las series seleccionadas)
+2- Escucho el click del botón
+3- Recojo el valor del input
+4- Petición al servidor con la nueva url
+5- Por cada serie contenida en el resultado de la url
+    Pinto la serie en el html
+
+*/
+
+const inputText = document.querySelector(".js-input");
+const buttonSearch = document.querySelector(".js-button");
+const contentSearch = document.querySelector(".js-content");
+let seriesList = [];
+
+function handleButton(){
+    const inputSearch = inputText.value;
+    fetch(`https://api.jikan.moe/v4/anime?q=${inputSearch}`)
+        .then((response)=>response.json())
+        .then(data => {
+            console.log(data);
+            seriesList = data.data;
+        const filteredSeries = seriesList.filter((serie)=>{
+            return serie.name === inputSearch;
+        })    
+            
+            
+        })
+}
+
+buttonSearch.addEventListener("click", handleButton);
+
